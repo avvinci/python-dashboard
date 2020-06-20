04e3f203f8472152ff0f2f77a72a80ae8167d8b9
avvinci <vinciabhinav7@gmail.com>
2020-06-20 23:39:33 +0530

    stats calculation added

--- hello/views.py
+++ hello/views.py
@@ -21,6 +21,7 @@ def index(request):
     dff = pd.DataFrame({"index": df["ML"].value_counts().sort_index().index, "cum_val":   df["ML"].value_counts(
     ).sort_index().cumsum(), "val":  df["ML"].value_counts().sort_index()})
     dff['perc'] = dff['cum_val'].apply(lambda x: x / dff.iloc[-1:]['cum_val'])
+   
     dd = []
     for i in range(0, df.shape[0]):
         k = (datetime.date.today() - datetime.timedelta(days=i))
@@ -29,13 +30,35 @@ def index(request):
     df = df.set_index('Data')
     df['cm_sum'] = df['ML'].cumsum()
     print('head df: ', df.head())
-    df_ml = df["ML"].values[:20]
-    df_index = df.index.values[:20]
+    df_ml = df["ML"].values[-10:]
+    df_index = df.index.values[-10:]
     print('head df: ', df_ml)
     print('index df: ', df_index)
 
-    return render(request, "index.html", {'courts': [20, 4, 23], 'club_name': 'club_name', 'df_ml': list(df_ml), 'df_index': list(df_index)
-                                          })
+    df_ml_30 = df["ML"].values[-30:]
+    df_index_30 = df.index.values[-30:]
+    print('head df: ', df_ml_30)
+    print('index df: ', df_index_30)
+
+    df_ml_full = df["ML"].values
+    df_index_full = df.index.values
+
+    dff = df.reset_index()
+    dff['Date'] = pd.to_datetime(dff['Data'])
+    dff.set_index('Date', inplace=True)
+
+    g = dff.groupby(pd.Grouper(freq="M"))
+    gg = g.sum()
+    month_full = gg["ML"].values
+    month_full_index = gg.index.values
+    print('head df: ', month_full)
+    print('index df: ', month_full_index)
+
+    return render(request, "index.html",
+                  {'courts': [20, 4, 23], 'club_name': 'club_name',
+                   'df_ml': list(df_ml), 'df_index': list(df_index)
+
+                   })
 
 
 def db(request):
